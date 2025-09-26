61449dfc200f8f00bd2b9328249e4169437b0de2
RAJKAMAL-02 <vrajkamal09@gmail.com>
2025-09-26 09:49:51 +0530

    Implemented Error Handling

--- ClientApp/Client.cs
+++ ClientApp/Client.cs
@@ -2,25 +2,56 @@ using System;
 using System.Net.Sockets;
 using System.Text;
 
-class Client{
+class Client
+{
     const string localHost = "127.0.0.1";
     const int PORT = 8085;
-    static void Main(String[] args){
-        TcpClient client = new TcpClient(localHost,PORT);
-        Console.WriteLine("connected to Server");
-        Console.Write("Enter string like SetA-One: ");
-        
-        string message = Console.ReadLine();
-        NetworkStream stream = client.GetStream();
-        byte[] data = Encoding.ASCII.GetBytes(message);
-        stream.Write(data,0,data.Length);
 
-        byte[] buffer = new byte[1024];
-        stream.Read(buffer,0,buffer.Length);
-        string response = Encoding.ASCII.GetString(buffer,0,buffer.Length);
-        Console.WriteLine(response);
-        client.Close();
-        
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (TcpClient client = new TcpClient(localHost, PORT))
+            {
+                Console.WriteLine("Connected to Server");
+
+                Console.Write("Enter string like SetA-One: ");
+                string message = Console.ReadLine();
+
+                // Send request
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    // Read response
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    if (bytesRead > 0)
+                    {
+                        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine("Server response:");
+                        Console.WriteLine(response);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No response from server.");
+                    }
+                }
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine("Socket error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unexpected error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Client closed.");
+        }
     }
-    
 }
--- ServerApp/Server.cs
+++ ServerApp/Server.cs
@@ -3,67 +3,107 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Collections.Generic;
+using System.Threading;
+
 class Server {
     static Dictionary<string,Dictionary<string,int>> sets = new Dictionary<string,Dictionary<string,int>>();
 
     static void Main(String[] args){
         const int PORT = 8085;
-        
 
-        sets.Add("SetA",new Dictionary<string,int> {{"One",1},{"Two",2}});
-        sets.Add("SetB",new Dictionary<string,int> {{"Three",3},{"Four",4}});
-        sets.Add("SetC",new Dictionary<string,int> {{"Five",5},{"Six",6}});
-        sets.Add("SetD",new Dictionary<string,int> {{"Seven",7},{"Eight",8}});
-        sets.Add("SetE",new Dictionary<string,int> {{"Nine",9},{"Ten",10}});
+        try
+        {
+            sets.Add("SetA",new Dictionary<string,int> {{"One",1},{"Two",2}});
+            sets.Add("SetB",new Dictionary<string,int> {{"Three",3},{"Four",4}});
+            sets.Add("SetC",new Dictionary<string,int> {{"Five",5},{"Six",6}});
+            sets.Add("SetD",new Dictionary<string,int> {{"Seven",7},{"Eight",8}});
+            sets.Add("SetE",new Dictionary<string,int> {{"Nine",9},{"Ten",10}});
 
-        TcpListener server = new TcpListener(IPAddress.Any,PORT);
-        server.Start();
-        Console.WriteLine("Server started at PORT: " + PORT);
-        while(true){
-            TcpClient client = server.AcceptTcpClient();
-            Console.WriteLine("Client Connected to Server");
-             Thread t = new Thread(()=>HandleClientRequest(client));
-             t.Start();
+            TcpListener server = new TcpListener(IPAddress.Any,PORT);
+            server.Start();
+            Console.WriteLine("Server started at PORT: " + PORT);
+
+            while(true){
+                try
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client Connected to Server");
+
+                    // Thread per client
+                    Thread t = new Thread(() => HandleClientRequest(client));
+                    t.Start();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error accepting client: " + ex.Message);
+                }
+            }
         }
-      
-        server.Stop();
-
+        catch (SocketException ex)
+        {
+            Console.WriteLine("Socket error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unexpected error: " + ex.Message);
+        }
     }
 
     static void HandleClientRequest(TcpClient client){
-        NetworkStream stream = client.GetStream();
-        byte[] buffer = new byte[1024];
-        int bytesRead = stream.Read(buffer,0,buffer.Length);
-        string clientRequest = Encoding.ASCII.GetString(buffer,0,bytesRead);
-        string response  = ProcessRequest(clientRequest);
+        try
+        {
+            using (client) 
+            using (NetworkStream stream = client.GetStream()) 
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer,0,buffer.Length);
 
-        // Console.WriteLine("Client Request: "+ clientRequest);
-        // Console.WriteLine("Client Response: "+ response);
-        byte[] responseData = Encoding.ASCII.GetBytes(response);
-        stream.Write(responseData,0,responseData.Length);
-        client.Close();
-    }
-    static string ProcessRequest(string message){
-        string[] subString = message.Split('-');
-    
-        if(subString.Length !=2) return "Empty";
-        string setName = subString[0].Trim();
-        string key = subString[1].Trim();
-        // Console.WriteLine("SetName: "+setName +" key: "+key);
-        if(sets.ContainsKey(setName) && sets[setName].ContainsKey(key)){
-            
-            int n = sets[setName][key];
-            StringBuilder sb = new StringBuilder();
-            for(int i=0;i<n;i++){
-                sb.AppendLine(DateTime.Now.ToString());
-                Thread.Sleep(1000);
+                if (bytesRead > 0)
+                {
+                    string clientRequest = Encoding.ASCII.GetString(buffer,0,bytesRead);
+                    string response  = ProcessRequest(clientRequest);
+
+                    byte[] responseData = Encoding.ASCII.GetBytes(response);
+                    stream.Write(responseData,0,responseData.Length);
+                }
+                else
+                {
+                    Console.WriteLine("Received empty request from client.");
+                }
             }
-            return sb.ToString();
-
         }
-        else{
-            return "Emtpy";
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error handling client request: " + ex.Message);
         }
     }
-    
+
+    static string ProcessRequest(string message){
+        try
+        {
+            string[] subString = message.Split('-');
+        
+            if(subString.Length !=2) return "Empty";
+            string setName = subString[0].Trim();
+            string key = subString[1].Trim();
+
+            if(sets.ContainsKey(setName) && sets[setName].ContainsKey(key)){
+                int n = sets[setName][key];
+                StringBuilder sb = new StringBuilder();
+                for(int i=0;i<n;i++){
+                    sb.AppendLine(DateTime.Now.ToString());
+                    Thread.Sleep(1000);
+                }
+                return sb.ToString();
+            }
+            else{
+                return "Emtpy";
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error in ProcessRequest: " + ex.Message);
+            return "Error";
+        }
+    }
 }
